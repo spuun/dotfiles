2bcc280bd4c6ac5cc0fc24b80e9876c10a4437f6
Jon Börjesson <jon@spuun.se>
2025-11-11 16:03:06 +0100

    Remove logging

--- .config/nvim/lua/spuun/init.lua
+++ .config/nvim/lua/spuun/init.lua
@@ -1,13 +1,6 @@
-print("spuun loading")
-
-
 require("spuun.packer")
 require("telescope").load_extension("git_worktree")
 
 require("spuun.options")
 require("spuun.remap")
 require("spuun.silver_searcher")
-
-
-
-print("spuun loaded")
